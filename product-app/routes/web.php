eb4ec7e2e02bf1755c76ea24c3fad51c1a83e7d0
salwazahramunir <salwazahramunir@gmail.com>
2023-06-24 15:08:41 +0700

    product app

--- product-app/routes/web.php
+++ product-app/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,13 +20,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Bikin routing ke dashboard
-Route::get('/dashboard', [DashboardController::class, 'index']);
+Route::group(
+    ['middleware' => ['auth']],
+    function () {
+        // Bikin routing ke dashboard
+        Route::get('/dashboard', [DashboardController::class, 'index']);
 
-// Buat Routing Produk
-Route::get('/products', [ProductController::class, 'index']);
-Route::get('/products/create', [ProductController::class, 'create']);
-Route::post('/products/store', [ProductController::class, 'store']);
-Route::get('/products/edit/{id}', [ProductController::class, 'edit']);
-Route::put('/products/update/{id}', [ProductController::class, 'update']);
-Route::get('/products/delete/{id}', [ProductController::class, 'destroy']);
+        // Buat Routing Produk
+        Route::get('/products', [ProductController::class, 'index']);
+        Route::get('/products/create', [ProductController::class, 'create']);
+        Route::post('/products/store', [ProductController::class, 'store']);
+        Route::get('/products/edit/{id}', [ProductController::class, 'edit']);
+        Route::put('/products/update/{id}', [ProductController::class, 'update']);
+        Route::get('/products/delete/{id}', [ProductController::class, 'destroy']);
+        Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    }
+);
+
+Auth::routes();
